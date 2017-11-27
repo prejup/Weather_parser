1993507c221c92c83f3494e5ced6528fc9f8a5c4
prejup <prejup@gmail.com>
2017-11-27 19:25:23 +0300

    123

---
+++ BD_connect.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+from PythonApplication1 import *
+from builtins import print
+import sqlite3
+
+
+# непонятная хрень,которая вроде как помогает с определителями дальше (хотя и без нее работает)
+paramstyle = sqlite3.paramstyle
+if paramstyle == 'qmark':
+    ph = "?"
+    ph1 = "?"
+    ph2 = "?"
+    ph3 = "?"
+    ph4 = "?"
+elif paramstyle == 'format':
+    ph = "%s"
+    ph1 = "%s"
+    ph2 = "%s"
+    ph3 = "%s"
+    ph4 = "%s"
+else:
+    raise Exception("Unexpected paramstyle: %s" % paramstyle)
+# конец непонятной хрени
+
+# Вызов id городов по их именам, если имен нет, то вызываются все
+def pars_cities (db_name, *kwargs):
+
+    # инициализация указатедя
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    # конец инициализации
+
+    project = []
+
+    if len(kwargs) > 0:
+        for kwarg in kwargs:
+
+            for row in c.execute ("select id from Cities where City_Name_Short  = ?", (str(kwarg),)):
+                project.append (row[0])
+    else:
+        for row in c.execute ("select id from Cities"):
+            project.append (row[0])
+    
+    c.close()
+    conn.close()
+
+    return(project)
+
+
+# функция по id города выдает link сайтов и id сайтов 
+def pars_data (db_name, db_city_id):
+
+    # инициализация указатедя
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    # конец инициализации
+
+    project = []
+
+    for row in c.execute ("select Refer, Site_id from Sites_links where Cities_id  = ?", (db_city_id,)):
+        project.append (row)
+
+    c.close()
+    conn.close()
+
+    return(project)
+   
+# конец функции
+
+
+# Функция записи парса в базу данных 
+def write_to_db (db_name, pars, db_city_id, db_syte_id):
+
+    # инициализация указатедя
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    # конец инициализации
+
+    # ------ цикл переноса записей из pars в базу
+    i=0 #счетчик прогонов для корректной вставки в имя столбца
+    for pars_1 in pars:
+
+        varebles = {"ph": str(i)+"_day_day",                    # имя столбца в БД 
+                    "ph1": str(i)+"_day_night",                 # имя столбца в БД 
+                    "ph2": str(pars_1['datetime']),             # переменная даты из парса
+                    "ph3": str(pars_1['day_temp']),             # переменная температуры из парса
+                    "ph4": str(pars_1['night_temp']),           # переменная температуры из парса
+                    "ph5": int(db_city_id),                     # id города для парса 
+                    "ph6": int(db_syte_id),                     # id сайта для парса
+                    }         
+  
+        c.execute("select * from Temps where Date ='%(ph2)s' and City_id = '%(ph5)s' and Site_id = '%(ph6)s'" % varebles) # проверка наличия текущей даты в базе
+
+        if c.fetchone() is None: # если даты нет, то ее нужно создать и внести данные
+        
+            c.execute ("""insert into 
+                          Temps ('Date', City_id, Site_id, '%(ph)s', '%(ph1)s') 
+                          values ('%(ph2)s','%(ph5)s','%(ph6)s', '%(ph3)s', '%(ph4)s')""" % varebles)
+            conn.commit()
+
+        else: # если дата есть, то данные нужно обновить
+
+            c.execute ("""update Temps
+                        set  '%(ph)s'  =  '%(ph3)s',
+                             '%(ph1)s'  =  '%(ph4)s' 
+                        where Date = '%(ph2)s' and
+                              City_id = '%(ph5)s' and 
+                              Site_id = '%(ph6)s'""" % varebles)
+            conn.commit()
+
+        i += 1
+
+
+    # закрытие указателя
+    c.close()
+    conn.close()
+
+
+# Обновление базы данных
+def refresh_bd (db_name, cities):
+
+    #для каждого города из списка
+    for db_city_id_ in cities:
+        sites = pars_data(db_name, db_city_id_) #забор link и id сайтов
+
+        # для каждого сайта из списка
+        for site in sites:
+            if site[1] == 1:
+                pars_ = parse_yandex(get_html(site[0]))
+
+            elif site[1] == 2:
+                pars_ = parse_gismeteo(get_html(site[0]))
+    
+            write_to_db(db_name_,pars_,db_city_id_, site[1])
+
+db_name_ = 'weather_db.sqlite3' # имя файла с базой
+cities_ = pars_cities(db_name_,)
+
+refresh_bd(db_name_, cities_)
---
+++ PythonApplication1.py
@@ -0,0 +1,120 @@
+
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return (response.read())
+
+
+#парсинг Яндекса
+def parse_yandex(html):
+    soup = BeautifulSoup (html,"lxml")
+    div = soup.find("", class_="forecast-briefly__days")
+
+    projects = []
+
+    for div_2 in div.find_all("div"):
+        times = div_2.find("a")
+        if times:
+            #достаю значение дневной температуры
+            day_temp = div_2.find("div", class_="temp forecast-briefly__temp forecast-briefly__temp_day")
+            day_temp = day_temp.find("span", class_="temp__value")
+
+            #достаю значение ночной температуры
+            night_temp = div_2.find("div", class_="temp forecast-briefly__temp forecast-briefly__temp_night")
+            night_temp = night_temp.find("span", class_="temp__value")
+
+            projects.append({
+                "datetime" : times.time["datetime"][0:10],
+                "day_temp" : day_temp.text,
+                "night_temp" : night_temp.text
+                })
+    
+    return projects
+
+
+#Парсинг гисметео
+def parse_gismeteo(html):
+    soup = BeautifulSoup (html,"lxml")
+    div_head = soup.find("div", class_="widget__container") # определил слой со всей информацией
+    div_date = div_head.find("div", class_="widget__row") #определил подслой с датами
+    div_val = div_head.find_all("div", class_="value") #создал список со значениями
+
+    projects = []
+
+    i = 0
+
+    for div_date_2 in div_date.find_all("div", class_="widget__item"):
+        
+
+        div_date_2_text = div_date_2.span.text.lstrip().rstrip() # удаляю пробелы вконце и сначала строки
+        div_date_2_text_num = div_date_2_text[0:2].rstrip() # вырезаю число
+ 
+        #для чисел из одной цифры добавляю нолик в начало
+        if len(div_date_2_text_num) < 2:
+            div_date_2_text_num = "0"+div_date_2_text_num
+
+        year = 2017 # костыли для перехода на следующий год
+        if div_date_2_text[-3:] == "янв":
+            div_date_2_text_mon = "01"
+            if j == '12':
+                year = year+1
+        elif div_date_2_text[-3:] == "фев":
+            div_date_2_text_mon = "02"
+        elif div_date_2_text[-3:] == "мар":
+            div_date_2_text_mon = "03"
+        elif div_date_2_text[-3:] == "апр":
+            div_date_2_text_mon = "04"
+        elif div_date_2_text[-3:] == "май":
+            div_date_2_text_mon = "05"
+        elif div_date_2_text[-3:] == "июн":
+            div_date_2_text_mon = "06"
+        elif div_date_2_text[-3:] == "июл":
+            div_date_2_text_mon = "07"
+        elif div_date_2_text[-3:] == "авг":
+            div_date_2_text_mon = "08"
+        elif div_date_2_text[-3:] == "сен":
+            div_date_2_text_mon = "09"
+        elif div_date_2_text[-3:] == "окт":
+            div_date_2_text_mon = "10"
+        elif div_date_2_text[-3:] == "ноя":
+            div_date_2_text_mon = "11"
+        elif div_date_2_text[-3:] == "дек":
+            div_date_2_text_mon = "12"
+            j = div_date_2_text_mon
+           
+ 
+    
+        div_val_2 = div_val[i]
+        day_temp = div_val_2.find("div", class_="maxt")
+        night_temp = div_val_2.find("div", class_="mint")
+
+        i += 1
+
+        projects.append({
+            "datetime" : "%s-%s-%s" % (str(year),div_date_2_text_mon,div_date_2_text_num),
+            "day_temp" : day_temp.text,
+            "night_temp" : night_temp.text
+            })          
+
+    return projects
+        
+def pars_mailru(html):
+    soup = BeautifulSoup (html,"lxml")
+    div_date_today = soup.find_all ('div', class_= 'information__header__left__date')
+    div_temp_today_day = soup.find_all ('div', class_='information__content__temperature')
+    div_temp_today_night
+    print(div_temp_today)
+    
+
+#    print("gismeteo:")
+#    print(parse_gismeteo(get_html("https://www.gismeteo.ru/weather-moscow-4368/10-days/")))
+#    print("\n \n yandex:")
+#    print(parse_yandex (get_html("https://yandex.ru/pogoda/krasnoyarsk")))
+
+
+pars_mailru(get_html('http://www.pogoda.mail.ru/prognoz/moskva/'))
+
+
